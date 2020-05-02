c0d84f91d731ab9fdef919c2d14fb524804ed765
Bobby <Bobby1985uk@gmail.com>
2020-05-02 10:22:41 +0100

    feat: add cats data source to apollo server

--- starter/src/index.js
+++ starter/src/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer, gql } = require("apollo-server");
 const DogsAPI = require("./data-sources/dogs-api");
+const CatsAPI = require("./data-sources/cats-api");
 
 const typeDefs = gql`
   type Dog {
@@ -7,8 +8,14 @@ const typeDefs = gql`
     breed: String
   }
 
+  type Cat {
+    name: String
+    breed: String
+  }
+
   type Query {
     dogs: [Dog]
+    cats: [Cat]
   }
 `;
 
@@ -17,11 +24,15 @@ const resolvers = {
     dogs: async (_source, _args, { dataSources }) => {
       return await dataSources.dogsAPI.getAllDogs();
     },
+    cats: async (_source, _args, { dataSources }) => {
+      return await dataSources.catsAPI.getAllCats();
+    },
   },
 };
 
 const dataSources = () => ({
   dogsAPI: new DogsAPI(),
+  catsAPI: new CatsAPI(),
 });
 
 const server = new ApolloServer({ typeDefs, resolvers, dataSources });
